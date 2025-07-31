5c9e6a53ce8308296952c2ae17e60b9497f6aafc
Leticia K <leticia.kauanny@escolar.ifrn.edu.br>
2025-07-31 13:54:50 -0300

    fofis

--- Assets/Scenes/ControleLampada.cs
+++ Assets/Scenes/ControleLampada.cs
@@ -9,7 +9,7 @@ public class ControleLampada : MonoBehaviour
     public Sprite imagemLampadaAcesa;  // A imagem da lâmpada acesa
     public Sprite imagemLampadaApagada; // A imagem da lâmpada apagada
 
-    public Image painelEscurecerTela; // NOVO: Aqui vai o seu PainelEscuro
+    public  GameObject painelEscurecerTela; // NOVO: Aqui vai o seu PainelEscuro
     public float tempoDeTransicao = 1.0f; // NOVO: Quanto tempo a tela leva pra escurecer/clarear
 
     public bool estaAcesa = false; // Começa com a lâmpada apagada
@@ -22,39 +22,28 @@ public class ControleLampada : MonoBehaviour
         if (estaAcesa)
         {
             button.sprite = imagemLampadaAcesa; // Troca a imagem do botão para a lâmpada acesa
+            button.color = new Color(255, 255, 255, 1);
             StartCoroutine(MudarCorDaTela(estaAcesa)); // Começa a clarear a tela (transparência total)
         }
         else
         {
             button.sprite = imagemLampadaApagada; // Troca a imagem do botão para a lâmpada apagada
+            button.color = new Color(255, 255, 255, 0.4f);
             StartCoroutine(MudarCorDaTela(estaAcesa)); // Começa a escurecer a tela (quase preto, mas não 100%)
         }
-        Debug.Log("Lâmpada está " + (estaAcesa ? "ACESA" : "APAGADA"));
+        // Debug.Log("Lâmpada está " + (estaAcesa ? "ACESA" : "APAGADA"));
     }
 
     // Função para mudar a cor da tela suavemente
     IEnumerator MudarCorDaTela(bool condiction)
     {
-        // Color corAtualDoPainel = painelEscurecerTela.color;
-        // float alphaInicial = corAtualDoPainel.a;
-        // float tempoPassado = 0f;
-
-        // while (tempoPassado < tempoDeTransicao)
-        // {
-        //     tempoPassado += Time.deltaTime; // Conta o tempo
-        //     float progresso = tempoPassado / tempoDeTransicao;
-        //     corAtualDoPainel.a = Mathf.Lerp(alphaInicial, alphaAlvo, progresso); // Calcula a nova transparência
-        //     painelEscurecerTela.color = corAtualDoPainel; // Aplica a nova transparência ao painel
-        //     yield return null; // Espera um pouquinho e continua no próximo momento
-        // }
-        // corAtualDoPainel.a = alphaAlvo; // Garante que a transparência chegue no valor certo
-        // painelEscurecerTela.color = corAtualDoPainel;
         if (condiction) {
+            yield return null;
             painelEscurecerTela.SetActive(!condiction);
         }
         else {
-            painelEscurecerTela.SetActive(!condiction);
             yield return null;
+            painelEscurecerTela.SetActive(!condiction);
         }
     }
 }
